35b381a3d9bb5c0fd04f0a29f6b35a1947e094a8
@Ash9311 <ashutoshmulki7@gmail.com>
2023-08-25 22:06:33 +0530

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/todo_app', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+
+// Middleware
+app.use(express.json());
+
+// Define routes here (CRUD operations for tasks)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
